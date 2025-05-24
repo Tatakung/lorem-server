b1672c9ac532a4769036c015d5db5d66fbb21bf2
Tatakung <tasawa.012@gmail.com>
2025-05-24 13:02:28 +0700

    แก้ไขหน้า dashboard ให้รองรับปุ่มเพิ่มข้อมูล

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { showTest, createImages } = require("../controller/auth");
 const router = express.Router();
-router.get("/test", showTest);
-// d;splfspfkcpsdlcsdpfksaldslffldfkds;flfd;
+router.get("/test", showTest); 
 router.post("/image", createImages);
 module.exports = router;
