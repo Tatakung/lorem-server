94a6b4edaa875a7895d9872296b47f325d01de2f
Tatakung <tasawa.012@gmail.com>
2025-05-24 13:01:33 +0700

    เพิ่มปุ่มดาวน์โหลด PDF หน้า report.blade.php

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,6 @@ const express = require("express");
 const { showTest, createImages } = require("../controller/auth");
 const router = express.Router();
 router.get("/test", showTest);
+// d;splfspfkcpsdlcsdpfksaldslffl
 router.post("/image", createImages);
 module.exports = router;
