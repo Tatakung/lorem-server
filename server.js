d3cb0d8c54460c195afacd2e7488009b6e9bd70d
Tatakung <tasawa.012@gmail.com>
2025-05-26 18:19:15 +0700

    ข้อความ commit เช่น เพิ่มฟีเจอร์ upload รูป

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 // Step 1 import ....
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const { readdirSync } = require("fs");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const { readdirSync } = require('fs')
+const cors = require('cors')
 // const authRouter = require('./routes/auth')
 // const categoryRouter = require('./routes/category')
 
 // middleware
-app.use(morgan("dev"));
-app.use(express.json({ limit: "20mb" }));
-app.use(cors());
-app.use(bodyParser.json());
+app.use(morgan('dev'))
+app.use(express.json({ limit: '20mb' }))
+app.use(cors())
 // app.use('/api',authRouter)
 // app.use('/api',categoryRouter)
-readdirSync("./routes").map((c) => app.use("/api", require("./routes/" + c)));
+readdirSync('./routes')
+    .map((c) => app.use('/api', require('./routes/' + c)))
 
 // Step 3 Router
 // app.post('/api',(req,res)=>{
@@ -25,4 +24,7 @@ readdirSync("./routes").map((c) => app.use("/api", require("./routes/" + c)));
 //     res.send('Jukkru 555+')
 // })
 // Step 2 Start Server
-app.listen(5001, () => console.log("Server is running on port 5001"));
+app.listen(5001,
+    () => console.log('Server is running on port 5001'))
+
+
